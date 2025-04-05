dc080e501056183bc445de7e10da535147dda821
Shy <shyguyx1412@gmail.com>
2025-04-05 21:56:40 +0000

    customize theme

--- src/Satellite.palette.js
+++
@@ -1,5 +0,0 @@
-const palette = JSON.parse(JSON.stringify(require('./Lunar.palette')))
-
-palette['BACKGROUND'] = '#262626'
-
-module.exports = palette
--- src/_build.js
+++ src/_build.js
@@ -3,14 +3,9 @@ const fs = require('fs')
 const base = require('./base.json')
 const themes = [
 	{
-		name: 'Lunar Pink',
+		name: 'Dark Pink',
 		type: 'dark',
 		palette: require('./Lunar.palette'),
-	},
-	{
-		name: 'Lunar Pink Satellite',
-		type: 'dark',
-		palette: require('./Satellite.palette'),
 	},
 ]
 
@@ -19,8 +14,8 @@ const build = (obj, palette) => {
 		if (typeof obj[k] === 'object') {
 			if (obj[k].length === undefined) build(obj[k], palette)
 			else obj[k].forEach((i) => build(i, palette))
-		} else if (typeof obj[k] === 'string' && obj[k].startsWith('LUNAR_')) {
-			const s = obj[k].substring('LUNAR_'.length).split('%')
+		} else if (typeof obj[k] === 'string' && obj[k].startsWith('$')) {
+			const s = obj[k].substring(1).split('%')
 			const color = palette[s[0]]
 			const alpha = s[1] ? s[1] : ''
 			obj[k] = `${color}${alpha}`
@@ -34,9 +29,10 @@ themes.forEach((theme) => {
 
 	out.name = theme.name
 	out.type = theme.type
+	
 
 	fs.writeFileSync(
-		`../themes/${theme.name.split(' ').join('')}.json`,
+		`./themes/${theme.name.split(' ').join('')}.json`,
 		JSON.stringify(out, null, 2)
 	)
 })
